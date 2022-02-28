d36d6690559ca4c617569288992454bd512f9c4c
Maria1055 <96591820+Maria1055@users.noreply.github.com>
2022-03-01 00:15:06 +0700

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,58 @@
+public class Person {
+
+    private String name;
+    private String middleName;
+    private String familyName;
+    private int age;
+    private int yearOfBirth;
+
+    public Person(String name, String middleName, String familyName, int age) {
+        this.name = name;
+        this.middleName = middleName;
+        this.familyName = familyName;
+        this.age = age;
+    }
+    public void printName() {
+        System.out.println("Имя: " + this.name);
+    }
+    public void printYear() {
+        System.out.println("Год рождения: " + (2022 - this.age));
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return familyName + " " + name + " " + middleName + ", " + age + " ";
+    }
+}
